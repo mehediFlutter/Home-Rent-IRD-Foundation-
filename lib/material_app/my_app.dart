fbc9a59771a616a8a2d536a73c5991bc11536d28
mehediFluttere <mehedii.edu.bd@gmail.com>
2025-01-08 22:34:38 +0600

    theme added

--- lib/material_app/my_app.dart
+++ lib/material_app/my_app.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       title: 'Home Rent IRD Foundation',
     );
   }
